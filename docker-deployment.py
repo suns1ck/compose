d6aee358593d9235d31640986486f963008bb0b1
Ilse <140922576+suns1ck@users.noreply.github.com>
2025-07-20 18:15:52 +1200

    Re-wrote docker-deployment.py
    Spent a couple weeks re-writing this script to remove all spaghetti code. Far easier to read and expand upon as necessary.

--- docker-deployment.py
+++ docker-deployment.py
@@ -1,43 +1,85 @@
 import os
+import subprocess
+import urllib
+import requests
 
-def directory():
-    dir_ask = input("Does a directory already exist for this application (Y/N)?: ")
-    if dir_ask.upper() == "Y":
-        global dir_location
-        dir_location = input("Please enter the full path to the application's existing directory (/example/directory): ")
-    elif dir_ask.upper() == "N":
-        dir_base = input("Please enter the base directory for your containers (/example): ")
-        dir_new = input("Please name the new directory you'd like to hold this application's data in (/example): ")
-        os.system(f"mkdir {dir_base}{dir_new}")
-        global created_dir
-        created_dir = (f"{dir_base}{dir_new}")
-    else:
-        print("Please enter a valid option.")
-        directory()
+compose_dict = {}
 
-def compose():
-    global compose_ask
-    compose_ask = input("Will you be deploying this container with an already locally existing docker-compose.yml file (Y/N)?: ")
-    if compose_ask.upper() == "Y":
-        input("Please place your existing compose file into the container's directory and then press enter.")
-    elif compose_ask.upper() == "N":
-        global compose_url
-        compose_url = input("Please enter the URL to your compose file: ")
-        os.system(f"cd {created_dir} && wget -q {compose_url}")
-    else:
-        print("Please enter a valid option.")
-        compose()
+def compose_collect(compose_dict):
 
-def deploy():
-    compose_file = input("Please enter the name and extension of your compose file (ex: docker-compose.yml): ")
-    if compose_ask.upper() == "Y":
-        os.system(f"cd {dir_location} && docker-compose -f {compose_file} up -d")
-    elif compose_ask.upper() == "N":
-        os.system(f"cd {created_dir} && wget -q {compose_url} && docker-compose -f {compose_file} up -d")
-    else:
-        print("Please enter a valid option.")
-        deploy()
+    # This function serves the purpose of collecting data on the containers/applications to be deployed. It loops through the collection process
+    # in the case that the user wants to deploy multiple containers at once.
+    
+    additional_deployment = None
 
-directory()
-compose()
-deploy()
+    while additional_deployment != "N":
+        application_name = input("Name of the application being deployed: ")
+        compose_location = input("Absolute path or URL for your compose file: ").strip()
+        deployment_location = input("Deployment directory for this container/application: ")
+    
+        # The below if statement checks if the directory exists and creates it if desired. Chose to create this as if the user will always want
+        # to create the directory if it does not already exist.
+
+        if not os.path.exists(deployment_location):        
+            try:
+                subprocess.run(["mkdir", "-p", deployment_location], check=True)
+                print(f"Successfully created directory ({deployment_location})")
+            except subprocess.CalledProcessError as failed_creation:
+                print("Failed to create directory")
+                exit()
+    
+        compose_dict[application_name] = {
+            "application_name" : application_name,
+            "compose_location" : compose_location,
+            "deployment_directory" : deployment_location
+        }
+        additional_deployment = input("Would you like to deploy an additional container/application (Y/N)?: ").strip().upper()
+    return compose_dict
+
+def compose_process(compose_dict):
+
+    # This function iterates through the compose_dict dictionary to grab the locations of the declared containers/applications.
+    # It then parses the locations, pulls the compose file if the location is one, and then proceeds to place the compose file(s)
+    # into the related deployment directory.
+
+    for application_name, details in compose_dict.items():
+        application_name = details["application_name"]
+        compose_location = details["compose_location"]
+        deployment_directory = details["deployment_directory"]
+
+        if urllib.parse.urlparse(compose_location).scheme:
+            print(f"Downloading remote compose file for {application_name} and placing in deployment directory.")
+            try:
+                response = requests.get(compose_location)
+                response.raise_for_status()  # Raise an error for bad responses
+                with open(os.path.join(deployment_directory, "docker-compose.yml"), "wb") as f:
+                    f.write(response.content)
+                print(f"Successfully placed {application_name} compose file.")
+            except requests.exceptions.RequestException as fail:
+                print(f"Failed to place {application_name} compose file: {fail}")
+                continue
+
+        else:
+            try:
+                print(f"Copying local compose file for {application_name} to it's deployment directory.")
+                subprocess.run(["cp", compose_location, os.path.join(deployment_directory, "docker-compose.yml")], check=True)
+            except subprocess.CalledProcessError as failed_copy:
+                print(f"Failed to copy {application_name} compose file to {deployment_directory}")
+                continue
+
+def compose_deploy(compose_dict):
+
+    # This function will iterate through the deployment location(s) and deploy all specified containers/applications.
+
+    for application_name, details in compose_dict.items():
+        deployment_directory = details["deployment_directory"]
+
+        try:
+            subprocess.run(["docker-compose", "up", "-d"], cwd=deployment_directory, check=True)
+        except subprocess.CalledProcessError as failed_deployment:
+            print(f"Your container failed to deploy: {failed_deployment}")
+
+
+compose_collect(compose_dict)
+compose_process(compose_dict)
+compose_deploy(compose_dict)
